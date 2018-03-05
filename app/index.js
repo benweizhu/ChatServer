9d8fd91907c74b734d39ec42231f44a7283db1a2
benweizhu <benweizhu@gmail.com>
2018-03-05 15:16:32 +0800

    add basic example

--- app/index.js
+++ app/index.js
@@ -1,6 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.post("/", function(req, res) {
+  res.send("Got a POST request");
+});
+
+app.put("/user", function(req, res) {
+  res.send("Got a PUT request at /user");
+});
+
+app.delete("/user", function(req, res) {
+  res.send("Got a DELETE request at /user");
+});
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
